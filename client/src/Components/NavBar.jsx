e2d847e7f64da3fd5a563d9a4d1aac1f366af3f1
Satyasuranjeet <satyajena911@gmail.com>
2025-04-10 13:39:10 +0530

    added +1

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,8 +1,15 @@
-import { Music2, User, LogOut, RadioTower } from 'lucide-react';
+import { Music2, User, LogOut, RadioTower, Goal ,Command} from 'lucide-react';
 
 const NavBar = ({ userName, onLogout }) => {
     const openBroadcast = () => {
         window.open('https://broadcast-music.onrender.com/', '_blank');
+    };
+
+    const openMood = () => {
+        window.open('https://mood-and-music-2-0-zw7d.vercel.app/', '_blank');
+    };
+    const Community = () => {
+        window.open('https://music-community-5srj.vercel.app/', '_blank');
     };
 
     return (
@@ -20,17 +27,33 @@ const NavBar = ({ userName, onLogout }) => {
                     
                     {userName && (
                         <>
+                            
+                            <button
+                                onClick={openBroadcast}
+                                className="flex items-center gap-2 px-4 py-2 rounded-full bg-blue-500/10 hover:bg-blue-500/20 text-blue-400 transition-all"
+                            >
+                                <RadioTower size={18}/>
+                                Broadcast
+                            </button>
+                            <button
+    onClick={Community}
+    className="flex items-center gap-2 px-4 py-2 rounded-full bg-green-500/10 hover:bg-green-500/20 text-green-400 transition-all"
+>
+    <Command size={18} />
+    Community
+</button>
+
+                            <button
+                                onClick={openMood}
+                                className="flex items-center gap-2 px-4 py-2 rounded-full bg-blue-500/10 hover:bg-blue-500/20 text-blue-400 transition-all"
+                            >
+                                <Goal size={18}/>
+                                Mood
+                            </button>
                             <div className="flex items-center gap-2">
                                 <User size={20} className="text-gray-400" />
                                 <span className="text-gray-300">Hi, {userName}!</span>
                             </div>
-                            <button
-                        onClick={openBroadcast}
-                        className="flex items-center gap-2 px-4 py-2 rounded-full bg-blue-500/10 hover:bg-blue-500/20 text-blue-400 transition-all"
-                    >
-                        <RadioTower size={18}/>
-                        Broadcast
-                    </button>
                             <button
                                 onClick={onLogout}
                                 className="flex items-center gap-2 px-4 py-2 rounded-full bg-red-500/10 hover:bg-red-500/20 text-red-400 transition-all"
